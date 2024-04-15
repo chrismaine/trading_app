5ef815c76920a16db2b07ddd1edeed7a378bda56
Wrelly Santos <shaleisantos0123@gmail.com>
2024-04-15 15:41:09 +0800

    Wrelly Santos

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:syncfusion_flutter_charts/charts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Stock Trading App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        fontFamily: 'Roboto', // Using Roboto font for consistency
+      ),
+      home: HomePage(),
+    );
+  }
+}
